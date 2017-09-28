b7970c68ce86b29c2a7fdf5484200c64504f6899
ashish0593 <ashish0593@gmail.com>
2017-09-28 15:47:31 -0400

    Removed Comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-// import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+
 
 import './App.css';
 import Header from "./header";
 import AppBody from "./AppBody";
 
-// const Input = () => (<div> Input form </div>)
-// const List = () => (<div> List Page </div>)
+
 
 class App extends Component {
   render() {
